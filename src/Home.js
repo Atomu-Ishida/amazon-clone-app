74ebee1547133682fb0b36fbe2e6d4ef6e5eb634
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-24 01:40:46 +0900

    create home page

---
+++ src/Home.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import './Home.css';
+import Product from './Product';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <img
+          className='home__image'
+          src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
+          alt='home'
+        />
+
+        <div className='home__row'>
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={5}
+          />
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={4}
+          />
+        </div>
+        <div className='home__row'>
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={5}
+          />
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={5}
+          />
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={5}
+          />
+        </div>
+        <div className='home__row'>
+          <Product
+            id={Math.random()}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+            rating={5}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
