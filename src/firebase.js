aa6b9725a46cda0df9f1e95ca58afb09740f0a27
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-23 20:30:04 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDt2NMBzZX656fjWVl0pr8-7_f4A48a30A',
+  authDomain: 'clone-7bfe5.firebaseapp.com',
+  projectId: 'clone-7bfe5',
+  storageBucket: 'clone-7bfe5.appspot.com',
+  messagingSenderId: '378995737733',
+  appId: '1:378995737733:web:ef53563358fc1a2f5a95ec',
+};
