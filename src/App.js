74a18852fbb03502e2e135c439fa5af8de672569
Atomu-Ishida <atomu.10.18@icloud.com>
2022-05-26 21:57:42 +0900

    payment page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from './Payment';
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -33,6 +34,7 @@ function App() {
           <Route path='/' element={[<Header />, <Home />]} />
           <Route path='/login' element={[<Login />]} />
           <Route path='/checkout' element={[<Checkout />]} />
+          <Route path='/orders' element={[<Header />, <Payment />]} />
         </Routes>
       </div>
     </Router>
